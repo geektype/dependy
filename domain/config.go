654752049a67ad67dde4e0cb316513dda328b417
Abdullah Ahmed <12223088+geektype@users.noreply.github.com>
2024-07-07 15:14:40 +0100

    docs: Add interface docs

--- domain/config.go
+++ domain/config.go
@@ -1,10 +1,17 @@
 package domain
 
+// Global configuration attributes
 type GlobalConfig struct {
+    // List of names of dependency managers to enable
 	DependencyManagers []string
+    // Default update policy use when one is not specified by the repository
 	DefaultPolicy      string
+    // Nmae of the remote GIT provider
 	RemoteGitProvider  string
+    // Prefix to use for merge request titles 
 	TitlePrefix        string
+    // Whether to delete the dependy branch after succesfully merging to main branch
 	RemoveSourceBranch bool
-	SquashCommits bool
+    // Whether to squash all commits of the dependy branch before merging
+	SquashCommits      bool
 }
--- domain/remote.go
+++ domain/remote.go
@@ -1,13 +1,30 @@
 package domain
 
-type Repository struct {
-	Id     string
-	Name   string
-	Url    string
-	Branch string
+// Handler for interacting with a remote GIT provider
+//
+// Responsible for interacting with relevant interfaces exposed by a GIT
+// provider i.e Github. Lean on remote provider's capabilities as much as
+// possible when implementing this interface to reduce internal complexity.
+type RemoteHandler interface {
+	// Provide a unique name to represent the remote provider
+	GetName() string
+
+	// Fetch repositories matching given filters and criteria
+    //
+    // TODO: Implement filters and search criteria
+	GetRepositories() ([]Repository, error)
+
+    // Create equivalent of a merge request in remote to merge dependy branch with main branch
+	CreateMergeRequest(repo Repository, sourceBranch string, targetBranch string) error
+
+    // Check if there is already an active merge request in the repository
+	CheckMRExists(repo Repository) (bool, error)
 }
 
-type RemoteHandler interface {
-    GetName() string
-	CreateMergeRequest(repo Repository, sourceBranch string, targetBranch string) error
+// A Git Repository provided by a remote provider
+type Repository struct {
+	Id     string // Identifier assigned by remote (not related to GIT)
+	Name   string // Human friendly repo name of the format <namespace>/<repo_name>. i.e geektype/dependy
+	Url    string // **HTTPS** remote URL for Repository
+	Branch string // Name of the designated main branch
 }
